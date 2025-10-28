ab9c6b07c709ba3fbd773144980eea61f843a626
phegde <prarthanahegde0702@gmail.com>
2025-10-28 22:42:02 +0530

    Movie page Modified

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import BannerSlider from '../components/shared/BannerSlider'
-import Navbar from '../components/shared/Navbar'
 
 const Movies = () => {
   return (
     <div>
-        <Navbar/>
         <BannerSlider/>
     </div>
   )
