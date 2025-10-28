f9c275029c2df865bb1a19f9278ea43c85912956
phegde <prarthanahegde0702@gmail.com>
2025-10-29 00:20:44 +0530

    Movie Page completed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,10 @@ import React from 'react'
 import BannerSlider from '../components/shared/BannerSlider'
 import Recommended from '../components/Recommended'
 import LiveEvents from '../components/shared/LiveEvents'
-import Navbar from '../components/shared/Navbar'
 
 const Home = () => {
   return (
     <div>
-    <Navbar/>
         <BannerSlider/>
         <Recommended/>
         <LiveEvents/>
--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import BannerSlider from '../components/shared/BannerSlider'
+import Moviefilters from '../components/Movies/Moviefilters'
+import MovieList from '../components/Movies/MoviesList'
 
 const Movies = () => {
   return (
     <div>
         <BannerSlider/>
+        <div className='flex flex-col md:flex-row bg-[#f5f5f5] min-h-screen 
+        md:px-[100px] pd-10 pt-8'>
+            <Moviefilters/>
+            <MovieList/>
+        </div>
     </div>
   )
 }
